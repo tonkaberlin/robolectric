3c0449930089ecd4c50ebaf1bb9cf0daac28860d
Roman Mazur <rmazur@stanfy.com.ua>
2013-11-20 20:39:14 +0200

    do all the operations on SQLite connections in a single thread, remove reset from ShadowSQLiteConnection

--- src/main/java/org/robolectric/Robolectric.java
+++ src/main/java/org/robolectric/Robolectric.java
@@ -1311,7 +1311,6 @@ public class Robolectric {
     ShadowStatFs.reset();
     ShadowTypeface.reset();
     ShadowCursorWindow.reset();
-    //ShadowSQLiteConnection.reset();
   }
 
   public static <T extends Activity> ActivityController<T> buildActivity(Class<T> activityClass) {
